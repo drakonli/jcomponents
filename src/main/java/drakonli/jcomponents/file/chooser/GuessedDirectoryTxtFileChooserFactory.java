fb6bc38644fe2660f817c1399b5e77120d52316b
drakonli <artur.drakonli@gmail.com>
2018-01-13 23:03:44 +0200

    - guessed directory chooser parameters to di

--- src/main/java/drakonli/jcomponents/file/chooser/GuessedDirectoryTxtFileChooserFactory.java
+++ src/main/java/drakonli/jcomponents/file/chooser/GuessedDirectoryTxtFileChooserFactory.java
@@ -7,18 +7,32 @@ import java.io.File;
 public class GuessedDirectoryTxtFileChooserFactory implements FileChooserFactoryInterface
 {
     private final String[] initialDirectoryGuesses;
+    private final String[] allowedExtensions;
+    private final String allowedExtensionsDescription;
+    private final String title;
 
-    public GuessedDirectoryTxtFileChooserFactory(String[] initialDirectoryGuesses)
+    public GuessedDirectoryTxtFileChooserFactory(
+            String[] initialDirectoryGuesses,
+            String[] allowedExtensions,
+            String allowedExtensionsDescription,
+            String title
+    )
     {
         this.initialDirectoryGuesses = initialDirectoryGuesses;
+        this.allowedExtensions = allowedExtensions;
+        this.allowedExtensionsDescription = allowedExtensionsDescription;
+        this.title = title;
     }
 
     @Override
     public FileChooser createFileChooser()
     {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open File");
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TEXT files (*.txt)", "*.txt");
+        fileChooser.setTitle(title);
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                this.allowedExtensionsDescription,
+                this.allowedExtensions
+        );
         fileChooser.getExtensionFilters().add(extFilter);
         fileChooser.setInitialDirectory(this.guessInitialDirectory());
 
